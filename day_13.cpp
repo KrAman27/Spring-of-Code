fd3261d8796d716796f310614302918b29e42a68
Aman Kumar <amankr2702@gmail.com>
2021-04-17 20:00:22 +0530

    DAy 1

---
+++ day_13.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(stack<int> s){
+    while(!(s.empty())){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+    cout<<"\n";
+}
+
+int largest(stack<int> s){
+    int max = s.top();
+    s.pop();
+
+    while(!(s.empty())){
+        if(max<s.top()){
+            max = s.top();
+            s.pop();
+        }
+        else{
+            s.pop();
+        }
+    }
+
+    return max;
+}
+
+int main(){
+    stack<int> s;
+
+    s.push(54);
+    s.push(64);
+    s.push(22);
+    s.push(12);
+    s.push(15);
+    s.push(2);
+
+    print(s);
+
+    cout<<"The largest data is : "<<largest(s)<<endl;
+}
